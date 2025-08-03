53fc2372553f745da1b777a24994690cc6ffda20
ServandoGonzl07 <servandogonzalez789@gmail.com>
2025-08-02 18:52:34 -0600

    Update officer.js

--- officer.js
+++ officer.js
@@ -1,21 +1,21 @@
 const officers = [
-    { name: "Alberto Cortez", role: "President", img: "../images/junta.png" },
-    { name: "David Bavi", role: "Vice President", img: "../images/junta.png" },
-    { name: "Servando Gonzalez", role: "Web Developer", img: "../images/junta.png" },
-    { name: "Camila Mendoza", role: "Fundraising Treasurer", img: "../images/junta.png" },
-    { name: "Karyme Saucedo", role: "Organizational Treasurer", img: "../images/junta.png" },
-    { name: "Emilie de la Garza", role: "Event Coordinator", img: "../images/junta.png" },
-    { name: "Valeria Flores x2", role: "Marketing Director", img: "../images/junta.png" },
-    { name: "Danna Avila", role: "Media Manager", img: "../images/junta.png" },
-    { name: "Renata Rodriguez", role: "Graphic Designer", img: "../images/junta.png" },
-    { name: "Carolina Soto", role: "Graphic Designer", img: "../images/junta.png" },
-    { name: "Enrique Berlanga", role: "Community Development", img: "../images/junta.png" },
-    { name: "Adrian Romero", role: "Academic Coordinator", img: "../images/junta.png" },
-    { name: "Anahi Silva", role: "Historian", img: "../images/junta.png" },
-    { name: "Mia Zermeno", role: "Historian", img: "../images/junta.png" },
-    { name: "Luis Aricheaga", role: "Recruiter", img: "../images/junta.png" },
-    { name: "Myra Benett", role: "Recruiter", img: "../images/junta.png" },
-    { name: "Donna Avila", role: "Music Coordinator", img: "../images/junta.png" }
+  { name: "Alberto Cortez", role: "President", img: "junta.png" },
+  { name: "David Bavi", role: "Vice President", img: "junta.png" },
+  { name: "Servando Gonzalez", role: "Web Developer", img: "junta.png" },
+  { name: "Camila Mendoza", role: "Fundraising Treasurer", img: "junta.png" },
+  { name: "Karyme Saucedo", role: "Organizational Treasurer", img: "junta.png" },
+  { name: "Emilie de la Garza", role: "Event Coordinator", img: "junta.png" },
+  { name: "Valeria Flores x2", role: "Marketing Director", img: "junta.png" },
+  { name: "Danna Avila", role: "Media Manager", img: "junta.png" },
+  { name: "Renata Rodriguez", role: "Graphic Designer", img: "junta.png" },
+  { name: "Carolina Soto", role: "Graphic Designer", img: "junta.png" },
+  { name: "Enrique Berlanga", role: "Community Development", img: "junta.png" },
+  { name: "Adrian Romero", role: "Academic Coordinator", img: "junta.png" },
+  { name: "Anahi Silva", role: "Historian", img: "junta.png" },
+  { name: "Mia Zermeno", role: "Historian", img: "junta.png" },
+  { name: "Luis Aricheaga", role: "Recruiter", img: "junta.png" },
+  { name: "Myra Benett", role: "Recruiter", img: "junta.png" },
+  { name: "Donna Avila", role: "Music Coordinator", img: "junta.png" }
 ];
 
 const officerGrid = document.getElementById('officerGrid');
