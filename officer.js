48989827022a6cee277a57498005dab5f415554f
ServandoGonzl07 <servandogonzalez789@gmail.com>
2025-08-08 12:23:32 -0600

    Update officer.js

--- officer.js
+++ officer.js
@@ -1,38 +1,30 @@
 const officers = [
-  { name: "Alberto Cortez", role: "President", img: "junta.png" },
-  { name: "David Bavi", role: "Vice President", img: "junta.png" },
-  { name: "Servando Gonzalez", role: "Web Developer", img: "junta.png" },
-  { name: "Camila Mendoza", role: "Fundraising Treasurer", img: "junta.png" },
-  { name: "Karyme Saucedo", role: "Organizational Treasurer", img: "junta.png" },
-  { name: "Emilie de la Garza", role: "Event Coordinator", img: "junta.png" },
-  { name: "Valeria Flores x2", role: "Marketing Director", img: "junta.png" },
-  { name: "Danna Avila", role: "Media Manager", img: "junta.png" },
-  { name: "Renata Rodriguez", role: "Graphic Designer", img: "junta.png" },
-  { name: "Carolina Soto", role: "Graphic Designer", img: "junta.png" },
-  { name: "Enrique Berlanga", role: "Community Development", img: "junta.png" },
-  { name: "Adrian Romero", role: "Academic Coordinator", img: "junta.png" },
-  { name: "Anahi Silva", role: "Historian", img: "junta.png" },
-  { name: "Mia Zermeno", role: "Historian", img: "junta.png" },
-  { name: "Luis Arechiga", role: "Recruiter", img: "junta.png" },
-  { name: "Myra Benett", role: "Recruiter", img: "junta.png" },
-  { name: "Donna Avila", role: "Music Coordinator", img: "junta.png" }
+    { name: "Alberto Cortez", role: "President", img: "../images/blank.png" },
+    { name: "David Bavi", role: "Vice President", img: "../images/blank.png"},
+    { name: "Servando Gonzalez", role: "Web Developer", img: "../images/blank.png" },
+    { name: "Camila Mendoza", role: "Fundraising Treasurer", img: "../images/blank.png" },
+    { name: "Karyme Saucedo", role: "Organizational Treasurer", img: "../images/blank.png"},
+    { name: "Emilie de la Garza", role: "Event Coordinator", img: "../images/blank.png"},
+    { name: "Valeria Flores", role: "Marketing Director", img: "../images/blank.png"},
+    { name: "Danna Avila", role: "Media Manager", img: "../images/blank.png"},
+    { name: "Renata Rodriguez", role: "Graphic Designer", img: "../images/blank.png"},
+    { name: "Carolina Soto", role: "Graphic Designer", img: "../images/blank.png"},
+    { name: "Enrique Berlanga", role: "Community Development", img: "../images/blank.png"},
+    { name: "Adrian Romero", role: "Academic Coordinator", img: "../images/blank.png"},
+    { name: "Anahi Silva", role: "Historian", img: "../images/blank.png"},
+    { name: "Mia Zermeno", role: "Historian", img: "../images/blank.png" },
+    { name: "Luis Aricheaga", role: "Recruiter", img: "../images/blank.png" },
+    { name: "Myra Benett", role: "Recruiter", img: "../images/blank.png" },
+    { name: "Danna Avila", role: "DJ", img: "../images/blank.png" },
 ];
 
 const officerGrid = document.getElementById('officerGrid');
-
-officers.forEach(officer => {
-    officerGrid.innerHTML += `
-    <div class="officer-div">
-        <div class="flex-inside">
-            <div class="officer-inside">
-                <img class="officer-pic" src="blank.png" alt="${officer.name}">
-            </div>
-            <div class="officer-text">
-           <h1>${officer.role}</h1>
-<p class="officer-name">${officer.name}</p>
-            <p></p>
-            </div>
+officerGrid.innerHTML = officers.map(officer => `
+    <div class="officer-card">
+        <div class="officer-role">${officer.role}</div>
+        <img class="officer-pic" src="${officer.img}" alt="${officer.name}">
+        <div class="officer-name">
+        ${officer.name}
         </div>
     </div>
-    `;
-});
+`).join('');
